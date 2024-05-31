9a383d5eb72340f9ae2a3548bf3fe3b3a9f207d0
Pascal <72224389+Evolinox@users.noreply.github.com>
2024-05-31 10:21:50 +0200

    Made Navbar Modular

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("/components/navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error fetching the navbar:', error));
+  fetch('/components/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error fetching the navbar:', error));
+});
