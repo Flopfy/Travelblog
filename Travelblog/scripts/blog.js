5521606ffd7ecf90777bc0fbcf2326936d63ccd9
Flopfy <flo.pfitzinger.23@lehre.mosbach.dhbw.de>
2024-06-15 13:54:15 +0200

    carousel

---
+++ Travelblog/scripts/blog.js
@@ -0,0 +1,44 @@
+function createBlogPage() {
+  const blogId = "B02E-ZU8F-K3BM-QZT3"
+  const headerImgDiv = document.getElementById('blogHeaderImg');
+
+  fetch("/resources/blogData.json")
+  .then(response => response.json())
+  .then(data => {
+    const entries = data.blogEntries;
+
+    for (let post of entries) {
+      if (blogId == post['blogId']) {
+        const headerImg = document.createElement('img');
+        headerImg.className = "headerImg";
+        headerImg.src = post['headerPicUrl']
+        headerImg.alt = post['headerPicAlt']
+        headerImg.summary = post['summary']
+        headerImgDiv.appendChild(headerImg);
+
+        for (let paragraph of post.paragraphs) {
+          console.log(paragraph['header'])
+          console.log(paragraph['text'])
+        }
+      }
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching the JSON file:', error);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("../../components/navbar.html")
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('navbar-container').innerHTML = data;
+      })
+      .catch(error => console.error('Error fetching the navbar:', error));
+    fetch('../../components/footer.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('footer-container').innerHTML = data;
+      })
+      .catch(error => console.error('Error fetching the navbar:', error));
+  });
